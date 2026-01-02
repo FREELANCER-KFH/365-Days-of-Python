61c2920517ca581d9092eb3b6489a945c41b843d
Kevin Feliz Henriquez <108124473+FREELANCER-KFH@users.noreply.github.com>
2026-01-01 23:48:47 -0400

    Create 001-hello-world.py

---
+++ 01-Fundamentos/001-hello-world.py
@@ -0,0 +1,7 @@
+# Fundamento: Variables y Funciones básicas
+def greet_user(name):
+    greeting = "Hola"
+    print(f"{greeting}, {name}! Bienvenido al reto 365.")
+
+user_name = "FREELANCER-KFH"
+greet_user(user_name)
